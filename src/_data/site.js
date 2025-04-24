511333e9a48d778cee0c60060646d5567fe92587
Toby Leftly <toby@leftly.me>
2025-04-23 21:42:13 -0400

    more env

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,13 +1,17 @@
-module.exports = {
-	"title": "Cornerstone Family Care",
-	"description": "Minimal boilerplate for new projects built with Eleventy, Tailwind, PostCSS and esbuild",
-	"url": "https://cornerstonecare.ca",
-	"baseurl": "/cornerstone",
-	"author": "Toby Leftly",
-	"meta_data": {
-		"twitter": "@chrissy_dev",
-		"opengraph_default": "/static/img/opengraph-default.webp"
-	},
-	env: isProd ? 'prod' : 'dev',
-  baseurl: isProd ? '/cornerstone' : '',
+module.exports = () => {
+	const isProd = process.env.ELEVENTY_ENV === 'production'
+
+	return {
+		"title": "Cornerstone Family Care",
+		"description": "Minimal boilerplate for new projects built with Eleventy, Tailwind, PostCSS and esbuild",
+		"url": "https://cornerstonecare.ca",
+		"baseurl": "/cornerstone",
+		"author": "Toby Leftly",
+		"meta_data": {
+			"twitter": "@tobyleftly",
+			"opengraph_default": "/static/img/opengraph-default.webp"
+		},
+		env: isProd ? 'prod' : 'dev',
+		baseurl: isProd ? '/cornerstone' : '',
+	}
 }
