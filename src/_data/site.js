854b97147fe6f1f12faa48cf2fd86d9e3c60de75
Toby Leftly <toby@leftly.me>
2025-04-23 20:26:14 -0400

    fix

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,6 +2,7 @@ module.exports = {
 	"title": "Cornerstone Family Care",
 	"description": "Minimal boilerplate for new projects built with Eleventy, Tailwind, PostCSS and esbuild",
 	"url": "https://cornerstonecare.ca",
+	"baseurl": "/cornerstone",
 	"author": "Toby Leftly",
 	"meta_data": {
 		"twitter": "@chrissy_dev",
